11836853529c09c150417bb0442ba1f4a5696308
Jefferson David Izquierdo Escobar <88057282+jeffersonizquierdo@users.noreply.github.com>
2023-01-16 10:16:20 -0500

    default port

--- models/Server.js
+++ models/Server.js
@@ -6,7 +6,7 @@ class Server {
 
     constructor(){
         this.app = express()    
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 3000;
         this.userPath = '/hello/users';
         this.authPath = '/hello/auth';
 
